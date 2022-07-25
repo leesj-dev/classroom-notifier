add574367f6200fa170758d3ecdab59baf3a5095
neutron <104193215+leesj-dev@users.noreply.github.com>
2022-07-25 21:45:06 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/Classroom\ Notifier.py 1 &
-python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/Classroom\ Notifier.py 2 &
-python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/Classroom\ Notifier.py 3 &
+python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/main.py 1 &
+python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/main.py 2 &
+python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/main.py 3 &
