522e330978b8d126edfb873cd893555e12ced711
leesj-dev <kevinjun0913@gmail.com>
2022-07-26 14:22:23 +0900

    added extra lines

--- src/run.bat
+++ src/run.bat
@@ -1,3 +1,5 @@
 start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 1"
 start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 2"
 start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 3"
+start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 4"
+start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 5"
--- src/run.sh
+++ src/run.sh
@@ -2,3 +2,5 @@
 python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/src/main.py 1 &
 python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/src/main.py 2 &
 python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/src/main.py 3 &
+python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/src/main.py 4 &
+python3 /Users/leesj/Documents/VS\ Code/classroom-notifier/src/main.py 5 &
