7827e2ac9c90c855ba43c57672a14454436020c4
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2022-07-28 21:32:52 +0900

    Update run.bat

--- src/run.bat
+++ src/run.bat
@@ -1,5 +1,5 @@
-start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 1"
-start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 2"
-start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 3"
-start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 4"
-start python3 "C:\Users\leesj\Documents\VS Code\classroom-notifier\src\main.py 5"
+start /B C:\Users\leesj\AppData\Local\Programs\Python\Python310\python.exe "C:/Users/leesj/Documents/VS Code/classroom-notifier/src/main.py" 1
+start /B C:\Users\leesj\AppData\Local\Programs\Python\Python310\python.exe "C:/Users/leesj/Documents/VS Code/classroom-notifier/src/main.py" 2
+start /B C:\Users\leesj\AppData\Local\Programs\Python\Python310\python.exe "C:/Users/leesj/Documents/VS Code/classroom-notifier/src/main.py" 3
+start /B C:\Users\leesj\AppData\Local\Programs\Python\Python310\python.exe "C:/Users/leesj/Documents/VS Code/classroom-notifier/src/main.py" 4
+start /B C:\Users\leesj\AppData\Local\Programs\Python\Python310\python.exe "C:/Users/leesj/Documents/VS Code/classroom-notifier/src/main.py" 5
